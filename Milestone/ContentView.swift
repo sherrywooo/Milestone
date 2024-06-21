c155702d1563a96539816d6241bcd2b28407d078
sherrywooo <xiangyi.wu@outlook.com>
2024-06-21 11:51:23 +0200

    Initial Commit

---
+++ Milestone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milestone
+//
+//  Created by Sherry Wu on 2024-06-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Milestone/MilestoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilestoneApp.swift
+//  Milestone
+//
+//  Created by Sherry Wu on 2024-06-21.
+//
+
+import SwiftUI
+
+@main
+struct MilestoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
